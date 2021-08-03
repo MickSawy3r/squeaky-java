a4c0c859f3ddedbaeb3d32a798bb0dec8ff7db40
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-08-03 22:39:17 +0300

    added annotation processor

---
+++ app/src/androidTest/java/de/sixbits/squeakyjava/PaymentMethodsListAcceptanceTest.java
@@ -0,0 +1,16 @@
+package de.sixbits.squeakyjava;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.runner.AndroidJUnitRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class PaymentMethodsListAcceptanceTest {
+
+    @Test
+    public void testPaymentMethod() {
+
+    }
+}
