6ef8a64e115062420559b82879be0da526cc3da5
mohammad <mohammad.al.kalaleeb@gmail.com>
2021-08-08 15:18:14 +0300

    added status code to the mock interceptor

--- app/src/sharedTest/java/de/sixbits/squeakyjava/utils/NetworkTestHelper.java
+++ app/src/sharedTest/java/de/sixbits/squeakyjava/utils/NetworkTestHelper.java
@@ -1,5 +1,8 @@
 package de.sixbits.squeakyjava.utils;
 
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import okhttp3.Interceptor;
@@ -9,18 +12,24 @@ import okhttp3.ResponseBody;
 
 public class NetworkTestHelper {
     @NotNull
-    public static Interceptor provideMockInterceptor(String fileName) {
+    public static Interceptor provideMockInterceptor(String fileName, Integer statusCode) {
         return chain -> {
             String payload = FileTestHelpers.loadJson(fileName);
 
             return chain.proceed(chain.request())
                     .newBuilder()
-                    .code(200)
+                    .code(statusCode)
                     .protocol(Protocol.HTTP_2)
                     .message(payload)
                     .body(ResponseBody.create(payload, MediaType.parse("application/json")))
                     .addHeader("content-type", "application/json")
                     .build();
         };
+    }
+
+    @NonNull
+    @Contract(pure = true)
+    public static Interceptor provideMockInterceptor(String fileName) {
+        return provideMockInterceptor(fileName, 200);
     }
 }
